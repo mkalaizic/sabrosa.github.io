f219accacb56379f2111f5cc6cfa34a8cfcecade
mkalaizic <kalaizicmercedes@gmail.com>
2022-04-26 11:54:56 -0400

    matched styling

--- app.js
+++ app.js
@@ -9,7 +9,10 @@ var myfunc = setInterval(function () {
   var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
   var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+  var seconds = Math.floor((timeleft % (1000 * 60)) / 1000).toLocaleString(
+    'en-US',
+    { minimumIntegerDigits: 2, useGrouping: false }
+  );
 
   // Result is output to the specific element
   document.getElementById('days').innerHTML = days;
