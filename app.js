c7f881960eb85edd085dd88732961bfdcdb49f9c
mkalaizic <kalaizicmercedes@gmail.com>
2022-05-03 17:38:49 -0400

    fix: centered content

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ var myfunc = setInterval(function () {
 const switcher = document.getElementById('corazon');
 
 switcher.addEventListener('click', function () {
-  document.body.classList.toggle('light-theme');
+  document.body.classList.toggle('dark-theme');
 
   // var className = document.body.className;
   // if (className == 'dark-theme') {
