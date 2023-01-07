f1d87d34991a56dc6b657702e5cfb1980c08ebcf
mkalaizic <kalaizicmercedes@gmail.com>
2023-01-07 12:56:17 -0500

    fixing issues

--- src/SabrosaApp.tsx
+++ src/SabrosaApp.tsx
@@ -9,13 +9,52 @@ const SabrosaAppWrapper = styled.div`
   background-color: #fefefd;
   margin: -10px;
   height: 110vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   .image-wrapper {
     display: flex;
     justify-content: center;
     align-items: center;
-    margin-top: -150px;
+    margin-top: -300px;
     flex-direction: column;
   }
+  .square-wrapper {
+    height: 100%;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 1;
+  }
+`;
+
+const createBox = keyframes`
+   from {
+    transform: scale(0);
+  }
+  to {
+    transform: scale(1);
+  }
+`;
+
+const StyledSquare = styled.span`
+  height: 120vh;
+  width: 220vh;
+  border: 1px solid violet;
+  animation: ${createBox} 4s infinite;
+`;
+
+const StyledButton = styled.button`
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+  background-color: #e2560a;
+  border-radius: 4px;
+  border: none;
+  height: 40px;
+  width: 150px;
+  margin-top: 30px;
+  color: #fdfefd;
 `;
 const rotate = keyframes`
   from {
@@ -25,10 +64,11 @@ const rotate = keyframes`
     transform: rotate(360deg);
   }
 `;
+
 const Rotate = styled.div`
   margin-top: -250px;
   display: inline-block;
-  animation: ${rotate} 2s linear infinite;
+  animation: ${rotate} 4s linear infinite;
   padding: 5px;
   font-size: 1.2rem;
 `;
@@ -36,14 +76,21 @@ const Rotate = styled.div`
 export const SabrosaApp: React.FC = () => {
   return (
     <SabrosaAppWrapper>
-      <span>Hola a todes</span>
+      {/* <div className='square-wrapper'> */}
+      {/* <StyledSquare></StyledSquare> */}
+      {/* </div> */}
       <div className='image-wrapper'>
         <img src={Logo} width='400px' />
-
         <Rotate>
-          <img src={Sabro5} width='400px' />
+          <img src={Sabro5} width='300px' />
         </Rotate>
       </div>
+      <StyledButton
+        onClick={() => {
+          console.log('here');
+        }}>
+        Get tickets
+      </StyledButton>
       {/* <img src={Sabro3} width='400px' />
       <img src={Sabro4} width='400px' /> */}
     </SabrosaAppWrapper>
